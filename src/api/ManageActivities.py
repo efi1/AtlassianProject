5c737316570b6d30080c8658bf0dec668c8431c1
efi1 <efovadia@gmail.com>
2024-07-02 13:22:20 +0300

    change in tearDown functionality

---
+++ src/api/ManageActivities.py
@@ -0,0 +1,9 @@
+from atlassian.bitbucket import Cloud
+
+class ManageActivities:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def login(self, url):
+        pass
