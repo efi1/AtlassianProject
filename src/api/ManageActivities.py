3ec02d5b3e2be543f07791f711a31467dfa9c38d
efio at_work <efio.at.work@gmail.com>
2024-07-03 23:51:36 +0000

    src/api/ManageActivities.py edited online with Bitbucket

--- src/api/ManageActivities.py
+++
@@ -1,9 +0,0 @@
-from atlassian.bitbucket import Cloud
-
-class ManageActivities:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def login(self, url):
-        pass
